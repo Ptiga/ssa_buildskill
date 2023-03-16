ec26be16bc0fb73500f3b1c51b6516bd17faa139
Ptiga <tylerdurdenxp@gmail.com>
2023-03-16 17:29:22 +0100

    Premier envoi du projet

---
+++ src/main/java/com/example/SaintSeiyaBuildskill/SaintSeiyaBuildskillApplication.java
@@ -0,0 +1,13 @@
+package com.example.SaintSeiyaBuildskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintSeiyaBuildskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintSeiyaBuildskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SaintSeiyaBuildskill/SaintSeiyaBuildskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SaintSeiyaBuildskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintSeiyaBuildskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
